5970330164b487fee82a150de08b3c04556d5668
a very fake floordiv <real.floordiv@gmail.com>
2022-08-10 22:06:24 +0200

    updates
    since now using mux in main

--- src/cmd/api/main.go
+++ src/cmd/api/main.go
@@ -33,18 +33,28 @@ func RunURLShortener(cfg config.Config) error {
 	linkService := service.NewLinkService(linkRepo)
 	netHTTPController := controller.NewHTTPController(shortenerService, linkService)
 
-	http.HandleFunc("/shorten", func(w http.ResponseWriter, req *http.Request) {
-		switch req.Method {
-		case http.MethodGet:
-			netHTTPController.DoRedirect(w, req)
-		case http.MethodPost:
-			netHTTPController.CreateRedirect(w, req)
-		default:
-			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/shorten", func(w http.ResponseWriter, req *http.Request) {
+		if req.URL.Path != "/shorten" {
+			http.Error(w, "not found", http.StatusNotFound)
 		}
+		if req.Method != http.MethodPost {
+			http.Error(w, "disallowed method", http.StatusMethodNotAllowed)
+			return
+		}
+
+		netHTTPController.CreateRedirect(w, req)
+	})
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		if req.Method != http.MethodPost {
+			http.Error(w, "disallowed method", http.StatusMethodNotAllowed)
+			return
+		}
+
+		netHTTPController.DoRedirect(w, req)
 	})
 
-	return http.ListenAndServe(cfg.Addr, nil)
+	return http.ListenAndServe(cfg.Addr, mux)
 }
 
 func renderConnectionURL(cfg config.Config) string {
